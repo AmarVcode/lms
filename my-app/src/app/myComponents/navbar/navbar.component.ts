8e9e493d8e47c3aebee81437063f2af363714c23
@amarvcode <amarvcode@gmail.com>
2022-12-22 10:58:23 +0530

    navbar completed

---
+++ my-app/src/app/myComponents/navbar/navbar.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent {
+public bool=false;
+
+menu(){
+  let div=document.getElementById("navbar");
+  if(this.bool===false){
+    this.bool=true;
+    div?.classList.add("afterclick")
+  }
+  else{
+    this.bool=false
+    div?.classList.remove("afterclick")
+
+  }
+}
+}
