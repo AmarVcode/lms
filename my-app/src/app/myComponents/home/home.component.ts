a1b536f4f14ad3b7d5fd7e4dbce9d4b43612afca
@amarvcode <amarvcode@gmail.com>
2022-12-28 14:09:28 +0530

    updated

--- my-app/src/app/myComponents/home/home.component.ts
+++ my-app/src/app/myComponents/home/home.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+public tho=""
+
+
+constructor(){
+  let thought=[
+    '“The purpose of our lives is to be happy.” - Dalai Lama',
+    '“Life is what happens when you’re busy making other plans.” - John Lennon',
+    '“Get busy living or get busy dying.” - Stephen King',
+    '“You only live once, but if you do it right, once is enough.” - Mae West',
+    '“Many of life’s failures are people who did not realize how close they were to success when they gave up.” - Thomas A. Edison',
+    '“If you want to live a happy life, tie it to a goal, not to people or things.” - Albert Einstein',
+    '“Never let the fear of striking out keep you from playing the game.” - Babe Ruth',
+    '“In order to write about life first, you must live it.” - Ernest Hemingway',
+    '“We are here to add what we can to life, not to get what we can from life.” - William Osler',
+    '"Work in Silence, Let Success Make Noise"'
+  ]
+
+  let ran:any=Math.random()
+  ran=ran*10
+  ran=Math.floor(ran)
+console.log(ran)
+  this.tho=thought[ran]
+}
+
 }
